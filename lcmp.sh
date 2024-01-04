cb3527ee94eea735429b46b7c4cc872b330ff884
妙正灰 <ivmmvobe@mf8.biz>
2024-01-05 00:55:40 +0800

    Revert "Update lcmp.sh"
    This reverts commit c7d0b97c80d69a81194f991fcb89c9e4b49669c4.

--- lcmp.sh
+++ lcmp.sh
@@ -520,7 +520,7 @@ if [ "$mariadb_ver" != "0" ]; then
 name = MariaDB
 baseurl = https://mirrors.aliyun.com/mariadb/yum/$mariadb_ver/rhel/\$releasever/\$basearch
 gpgkey = https://mirrors.aliyun.com/mariadb/yum/RPM-GPG-KEY-MariaDB
-module_hotfixes = 1
+enabled=1
 gpgcheck = 1
 EOF
             fi
